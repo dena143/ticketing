090af19e45281c4f37c4858d6e01d1c923399e41
Mochamad Dena Eka Putra <sica4822@gmail.com>
2022-03-24 16:39:11 +0700

    ticketing app first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,14 @@
+import express, { json, urlencoded } from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi There!");
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
